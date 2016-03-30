40f5bd823541a5b5500e906eb6151fa4a76ce95a
Uula Ranta <uula.ranta@gmail.com>
2016-03-30 14:00:20 +0300

    Add drawables and complete drawer navigation

--- app/src/main/java/fi/oulu/cse/iknowwhatyoudidaftersupper/MainActivity.java
+++ app/src/main/java/fi/oulu/cse/iknowwhatyoudidaftersupper/MainActivity.java
@@ -80,17 +80,18 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        // TODO: Navigate by switching fragments
+        if (id == R.id.nav_dashboard) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_tasks) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_calendar) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_map) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_groceries) {
+
+        } else if (id == R.id.nav_settings) {
 
         }
 
