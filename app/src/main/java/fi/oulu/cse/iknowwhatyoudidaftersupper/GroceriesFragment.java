150c9df88dc5663513b2eb8b9e7c360284a11176
Aapo <aapo.lehtosaari@pehtuec.com>
2016-04-09 19:37:13 +0300

    GroceryList, some layout problems still

--- app/src/main/java/fi/oulu/cse/iknowwhatyoudidaftersupper/GroceriesFragment.java
+++ app/src/main/java/fi/oulu/cse/iknowwhatyoudidaftersupper/GroceriesFragment.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
@@ -13,6 +20,9 @@ import android.view.ViewGroup;
  */
 public class GroceriesFragment extends Fragment {
 
+    private ArrayList<String> groceryList;
+    private ArrayAdapter<String> groceryAdapter;
+    private EditText GroceryInput;
 
     public GroceriesFragment() {
         // Required empty public constructor
@@ -26,7 +36,22 @@ public class GroceriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groceries, container, false);
+        View view = inflater.inflate(R.layout.fragment_groceries, container, false);
+        ListView listView=(ListView)view.findViewById(R.id.GroceryList);
+        String[] items = {};
+        groceryList = new ArrayList<>(Arrays.asList(items));
+        groceryAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item, R.id.txtitem, groceryList);
+        listView.setAdapter(groceryAdapter);
+        GroceryInput = (EditText)view.findViewById(R.id.GroceryInput);
+        Button AddBtn = (Button)view.findViewById(R.id.GroceryAddBtn);
+        AddBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String newItem = GroceryInput.getText().toString();
+                groceryList.add(newItem);
+                groceryAdapter.notifyDataSetChanged();
+            }
+        });
+        return view;
     }
 
 }
