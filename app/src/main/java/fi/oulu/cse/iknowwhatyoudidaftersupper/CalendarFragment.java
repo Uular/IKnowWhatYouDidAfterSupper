24a4bad5f01db7f8cdf2495b442a8879e2413bca
Uula Ranta <uula.ranta@indooratlas.com>
2016-05-02 18:40:36 +0300

    Add calendar view to calendar fragment

--- app/src/main/java/fi/oulu/cse/iknowwhatyoudidaftersupper/CalendarFragment.java
+++ app/src/main/java/fi/oulu/cse/iknowwhatyoudidaftersupper/CalendarFragment.java
@@ -1,11 +1,19 @@
 package fi.oulu.cse.iknowwhatyoudidaftersupper;
 
 
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -27,7 +35,32 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        WeekView wv = (WeekView) v.findViewById(R.id.calendar);
+        wv.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                // TODO: Fetch events by year and month
+                return new ArrayList<WeekViewEvent>();
+            }
+        });
+
+        wv.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+        wv.setEventLongPressListener(new WeekView.EventLongPressListener() {
+            @Override
+            public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+        return v;
     }
 
 }
